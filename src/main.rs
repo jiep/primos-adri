4460f17e9c1b9402527a539b047d9711df2e3687
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-11-20 18:07:40 +0000

    Fix format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use clap::{command, Parser};
 use regex::Regex;
-use clap::{Parser, command};
 
 fn primes(maximum: u64) -> Vec<u64> {
     let mut primes = vec![2];
@@ -21,14 +21,13 @@ fn primes(maximum: u64) -> Vec<u64> {
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[clap(short, long, default_value_t=100000)]
+    #[clap(short, long, default_value_t = 100000)]
     maximum: u64,
 }
 
 fn main() {
-
     let args = Args::parse();
-    
+
     let maximum: u64 = args.maximum;
     let is_primo_adri = Regex::new(r"[2,3,5,7]{4}7").unwrap();
 
